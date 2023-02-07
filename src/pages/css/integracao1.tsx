523ab9e920662e5774602707bcfc43b75b250efb
Marcos Sabatini <marcos.sabatini@ionsistemas.com.br>
2023-02-07 17:19:06 -0300

    css global

---
+++ src/pages/css/integracao1.tsx
@@ -0,0 +1,9 @@
+export default function Integracao() {
+	return (
+		<div className="integracao1">
+			<h1 className="red">Texto #01</h1>
+			<h1 className="blue">Texto #02</h1>
+			<h1 className="white">Texto #03</h1>
+		</div>
+	)
+}
