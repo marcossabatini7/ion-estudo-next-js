00fc2330d155bc697193b2713863ea7cf73e231a
Marcos Sabatini <marcos.sabatini@ionsistemas.com.br>
2023-02-07 17:23:34 -0300

    css por id

--- src/pages/css/integracao2.tsx
+++ src/pages/css/integracao2.tsx
@@ -2,10 +2,10 @@ import styles from './integracao2.module.css'
 
 export default function Integracao() {
 	return (
-		<>
+		<div id={styles.integracao2}>
 			<h1 className={styles.red}>Texto #01</h1>
 			<h1 className={styles.blue}>Texto #02</h1>
 			<h1 className={styles.white}>Texto #03</h1>
-		</>
+		</div>
 	)
 }
