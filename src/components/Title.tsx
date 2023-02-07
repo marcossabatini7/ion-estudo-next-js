2523a07f57054c410e098f252b055139b175eeb6
Marcos Sabatini <marcos.sabatini@ionsistemas.com.br>
2023-02-07 16:48:52 -0300

    props opcionais

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,11 +1,20 @@
 export default function Title({
 	title,
 	subtitle,
+	small,
 }: {
 	title: string
 	subtitle: string
+	small?: boolean
 }) {
-	return (
+	console.log({ small })
+
+	return small ? (
+		<>
+			<h3>{title}</h3>
+			<h4>{subtitle}</h4>
+		</>
+	) : (
 		<>
 			<h1>{title}</h1>
 			<h2>{subtitle}</h2>
--- src/pages/basicos/usando-titulo.tsx
+++ src/pages/basicos/usando-titulo.tsx
@@ -1,5 +1,14 @@
 import Title from '@/components/title'
 
 export default function UsandoTitulo() {
-	return <Title title="Title" subtitle="Subtitle" />
+	return (
+		<>
+			<Title title="Página de Cadastro" subtitle="Informe seu nome e e-mail" />
+			<Title
+				title="Página de Login"
+				subtitle="Informe seu e-mail e senha"
+				small={true}
+			/>
+		</>
+	)
 }
