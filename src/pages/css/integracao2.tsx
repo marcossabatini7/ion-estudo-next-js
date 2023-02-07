320d84f5ae46711f851224d21065070d14947c8e
Marcos Sabatini <marcos.sabatini@ionsistemas.com.br>
2023-02-07 17:19:14 -0300

    css modular

---
+++ src/pages/css/integracao2.tsx
@@ -0,0 +1,11 @@
+import styles from './integracao2.module.css'
+
+export default function Integracao() {
+	return (
+		<>
+			<h1 className={styles.red}>Texto #01</h1>
+			<h1 className={styles.blue}>Texto #02</h1>
+			<h1 className={styles.white}>Texto #03</h1>
+		</>
+	)
+}
